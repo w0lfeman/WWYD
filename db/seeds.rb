01be92e88b48e2c327873bfd52db104cb7a4e7e1
github username <github email address>
2019-01-03 17:39:20 -0600

    second commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Post.destroy_all
+Comment.destroy_all
 
-User.create(name: "David", handle: "wolfeman", description: "He follows the rules till he breaks them, MUHHAWHAWHAWHAW!")
-User.create(name: "Mike", handle: "dropTheMike", description: "Everybody, everbody, everybody wanna be like Mike!")
 
-Post.create(title: "Grocery", item: "Get milk", comment: "you should really think about getting milk you SOB.")
-Post.create(title: "Gym", item: "Pay dues", comment: "You'll stay healthy of you pay the dues so you can workout.")
 
-Comment.create(comment:"Got milk?")
-Comment.create(comment:"Don't die at the gym.")
+User.create!(name: "David", handle: "wolfeman", description: "He follows the rules till he breaks them, MUHHAWHAWHAWHAW!")
+User.create!(name: "Mike", handle: "dropTheMike", description: "Everybody, everbody, everybody wanna be like Mike!") 
+Post.create!(title: "Grocery", user_id: User.first.id, item: "Get milk", comment: "You should really think about getting milk you SOB.")
+Post.create!(title: "Gym", user_id: User.second.id, item: "Pay dues", comment: "You'll stay healthy of you pay the dues so you can workout.")
+
+Comment.create!(comment:"Got milk?", post_id: Post.first.id, user_id: User.first.id)
+Comment.create!(comment:"Don't die at the gym.", post_id: Post.second.id, user_id: User.second.id)
